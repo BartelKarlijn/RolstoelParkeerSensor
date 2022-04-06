3f593f1128414e2d78586a1d25a584ca66785204
BartelKarlijn <legoreus@gmail.com>
2022-04-06 21:43:14 +0200

    led berekenen in functie

--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,29 @@
 */
 
 // defines pins numbers
-#define trigPinL 6
-#define echoPinL 7
+#define led_pin 3
 #define trigPinR 4
 #define echoPinR 5
-// defines variables
-long duration;
-int distanceL, distanceR;
+#define trigPinL 6
+#define echoPinL 7
 
 // How many leds in your strip?
 #define NUM_LEDS 32
-#define DATA_PIN 3
 #define BRIGHTNESS 50
-CRGB leds[NUM_LEDS];
 
-#define DistParking 40
+// ideale afstand
+#define AfstandMinimum 5    //Alles kleiner wordt 5cm
+#define AfstandMaximum 100  //Alles groter wordt 100cm
+#define AfstandIdealVan 35  // Hiertussen is goed geparkeerd
+#define AfstandIdealTot 45
+
+// defines variables
+long duration;
+int distanceL, distanceR;
+int DistParking;   // DELETEME DELETEME
 int ledL, ledR;
 
+CRGB leds[NUM_LEDS];
 
 void setup()
 {
@@ -35,14 +41,34 @@ void setup()
   pinMode(echoPinR, INPUT);  // Sets the echoPin as an Input
   Serial.begin(9600);       // Starts the serial communication
 
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+  FastLED.addLeds<NEOPIXEL, led_pin>(leds, NUM_LEDS);  // GRB ordering is assumed
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
   FastLED.clear();  // clear all pixel data
 }
 
-void loop()
-{
+void BerekenLED(int distance, int ledOK, int ledVer) {
+  int led ;
+  if (distance <= AfstandMinimum) {  //Veel te kortbij
+    leds[ledVer] = CRGB::Red;
+  }
+  else if (distance < AfstandIdealVan) {  // Te kortbij
+    led = map(distance, AfstandMinimum, AfstandIdealVan, ledVer, ledOK);
+    leds[led] = CRGB::Red;
+  }
+  else if (distance <= AfstandIdealTot) { //Ideaal
+    leds[ledOK] = CRGB::Green;
+  }
+  else if (distance < AfstandMaximum) {  // Te ver
+    led = map(distance, AfstandIdealTot, AfstandMaximum, ledOK, ledVer);
+    leds[led] = CRGB::Blue;
+  }
+  else {  //Veel te ver
+    leds[ledVer] = CRGB::Blue;
+  }
+}
+
+void loop() {
   //Links
   delay(100);    //wachten tot andere golf weg is
   // Clears the trigPin
@@ -79,45 +105,13 @@ void loop()
 
 // Ledlampje aanzetten
   FastLED.clearData();
-  // Links
-  if (distanceL >= DistParking) {
-    // te ver af, dus Blauw
-    ledL = map(min(distanceL, DistParking * 2), DistParking * 2, DistParking, 0, NUM_LEDS/2 -1 );
-    leds[ledL] = CRGB::Blue;
-  }
-  else {
-    // te dicht bij, dus Rood
-    ledL = map(distanceL, 0, DistParking, 0, NUM_LEDS/2 -1 );
-    leds[ledL] = CRGB::Red;
-  }
-
-  // Rechts
-  if (distanceR >= DistParking) {
-    // te ver af, dus Blauw
-    ledR = map(min(distanceR, DistParking * 2), DistParking * 2, DistParking, NUM_LEDS-1, NUM_LEDS/2 );
-    leds[ledR] = CRGB::Blue;
-  }
-  else {
-    // te dicht bij, dus Rood
-    ledR = map(distanceR, 0, DistParking, NUM_LEDS-1, NUM_LEDS/2 );
-    leds[ledR] = CRGB::Red;
-  }
-
-// Groene lichtjes
-  if (  (distanceL <= DistParking + 3)
-    and (distanceL >= DistParking - 3)
-    and (distanceR <= DistParking + 3)
-    and (distanceR >= DistParking - 3) ) {
-  
-      FastLED.clearData();
-      leds[15] = CRGB::Green;
-      leds[16] = CRGB::Green;
-    }
+  BerekenLED(distanceL, NUM_LEDS/2-1, 0);
+  BerekenLED(distanceR, NUM_LEDS/2, NUM_LEDS);
+  FastLED.show();
 
   Serial.print(" LedL: ");
   Serial.print(ledL);
   Serial.print(" - LedR: ");
   Serial.println(ledR);
   
-  FastLED.show();
 }
